84e7374006a88db4e5d470b5412d2feec9666d91
OmarOrtVal <23308060610674@cetis61.edu.mx>
2026-02-26 11:57:23 -0600

    primer comit

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from ovod-23308060610674imagen!")
+import flet as ft 
 
-
-if __name__ == "__main__":
-    main()
+def main(page:ft.Page):
+    page.title = "Imagen"
+    page.add(ft.Text("Hello, World!"))
+    
+    page.add(ft.Text(
+        value="Hola Mundo",
+        size=24,
+        color=ft.Colors.BLUE,
+        weight=ft.FontWeight.BOLD,
+        italic=False,
+        text_align=ft.TextAlign.CENTER,
+        max_lines=2,
+        overflow=ft.TextOverflow.ELLIPSIS
+    ))
+    
+    page.add(ft.Image(
+        src="https://picsum.photos/id/1/200/300",
+        width=200,
+        height=300,
+        fit="cover",
+        border_radius=ft.BorderRadius.all(10),
+        repeat=ft.ImageRepeat.NO_REPEAT,
+    ))
+    
+    page.add(ft.Divider(height=20, thickness=2, color=ft.Colors.GREY_400))
+    page.add(ft.Row([
+        ft.VerticalDivider(width=10, thickness=2, color=ft.Colors.GREY_400)
+    ]))
+    
+ft.run(main)
